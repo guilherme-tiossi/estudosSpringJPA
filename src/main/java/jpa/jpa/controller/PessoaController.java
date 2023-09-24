e42840c0d4586c35924d3ee97a43f9365047115e
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-09-24 11:19:43 -0300

    adicionado sistema de deleção da api

--- src/main/java/jpa/jpa/controller/PessoaController.java
+++ src/main/java/jpa/jpa/controller/PessoaController.java
@@ -2,7 +2,9 @@ package jpa.jpa.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +30,14 @@ public class PessoaController {
     public List<Pessoa> buscarTodas() {
         return pessoaService.buscarTodas();
     }
+    
+    @GetMapping("/buscar/{id}")
+    public Pessoa buscarPorId(@PathVariable Long id) {
+        return pessoaService.buscarPorId(id);
+    }
 
-    @GetMapping("/ola")
-    public String getOla() {
-        return "Olá mundo!";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarPorId(@PathVariable Long id) {
+        pessoaService.deletarPorId(id);
     }
 }
--- src/main/java/jpa/jpa/servicos/PessoaService.java
+++ src/main/java/jpa/jpa/servicos/PessoaService.java
@@ -1,6 +1,7 @@
 package jpa.jpa.servicos;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,21 @@ public class PessoaService {
     @Autowired
     private PessoaRepository pessoaRepository;  
 
+
     public void salvarPessoa(@RequestBody Pessoa pessoa){
         pessoaRepository.save(pessoa);
     }
 
     public List<Pessoa> buscarTodas() {
         return pessoaRepository.findAll();
+    }
+
+    public Pessoa buscarPorId(Long id) {
+        Optional<Pessoa> Optionalpessoa = pessoaRepository.findById(id);
+        return Optionalpessoa.orElse(null); 
+    }
+
+    public void deletarPorId(Long id) {
+        pessoaRepository.deleteById(id);
     }
 }
