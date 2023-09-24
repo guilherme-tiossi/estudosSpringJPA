7ddc75fe15eeede50e081fb280ecb331520c01bc
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-09-24 11:30:53 -0300

    Finalizado projeto de API simples

--- src/main/java/jpa/jpa/controller/PessoaController.java
+++ src/main/java/jpa/jpa/controller/PessoaController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,12 @@ import jpa.jpa.servicos.PessoaService;
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
-    
+
     @Autowired
-    private PessoaService pessoaService;  
+    private PessoaService pessoaService;
 
     @PostMapping("/salvar")
-    public void salvarPessoa(@RequestBody Pessoa pessoa){
+    public void salvarPessoa(@RequestBody Pessoa pessoa) {
         pessoaService.salvarPessoa(pessoa);
     }
 
@@ -30,7 +31,7 @@ public class PessoaController {
     public List<Pessoa> buscarTodas() {
         return pessoaService.buscarTodas();
     }
-    
+
     @GetMapping("/buscar/{id}")
     public Pessoa buscarPorId(@PathVariable Long id) {
         return pessoaService.buscarPorId(id);
@@ -39,5 +40,10 @@ public class PessoaController {
     @DeleteMapping("/deletar/{id}")
     public void deletarPorId(@PathVariable Long id) {
         pessoaService.deletarPorId(id);
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public void atualizarPorId(@PathVariable Long id, @RequestBody Pessoa pessoa) {
+        pessoaService.atualizarPessoa(id, pessoa);
     }
 }
--- src/main/java/jpa/jpa/servicos/PessoaService.java
+++ src/main/java/jpa/jpa/servicos/PessoaService.java
@@ -5,20 +5,37 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import jakarta.persistence.EntityNotFoundException;
 import jpa.jpa.dominio.Pessoa;
 import jpa.jpa.repositorio.PessoaRepository;
 
-@Service 
+@Service
 public class PessoaService {
-    
+
     @Autowired
-    private PessoaRepository pessoaRepository;  
+    private PessoaRepository pessoaRepository;
 
-
-    public void salvarPessoa(@RequestBody Pessoa pessoa){
+    public void salvarPessoa(@RequestBody Pessoa pessoa) {
         pessoaRepository.save(pessoa);
+    }
+
+    public void atualizarPessoa(@PathVariable Long id, @RequestBody Pessoa pessoaAtualizada) {
+        Pessoa pessoaExistente = buscarPorId(id);
+
+        if (pessoaExistente != null) {
+            pessoaExistente.setNome(pessoaAtualizada.getNome());
+            pessoaExistente.setCpf(pessoaAtualizada.getCpf());
+            pessoaExistente.setSobrenome(pessoaAtualizada.getSobrenome());
+            pessoaExistente.setEmail(pessoaAtualizada.getEmail());
+            pessoaExistente.setTelefones(pessoaAtualizada.getTelefones());
+
+            pessoaRepository.save(pessoaExistente);
+        } else {
+            throw new EntityNotFoundException("Pessoa não encontrada com o ID: " + id);
+        }
     }
 
     public List<Pessoa> buscarTodas() {
@@ -27,10 +44,18 @@ public class PessoaService {
 
     public Pessoa buscarPorId(Long id) {
         Optional<Pessoa> Optionalpessoa = pessoaRepository.findById(id);
-        return Optionalpessoa.orElse(null); 
+        return Optionalpessoa.orElse(null);
     }
 
     public void deletarPorId(Long id) {
         pessoaRepository.deleteById(id);
     }
+
+    public void verifyIfExists(Long id) {
+        Optional<Pessoa> Optionalpessoa = pessoaRepository.findById(id);
+        if (!Optionalpessoa.isPresent()) {
+            throw new EntityNotFoundException("Pessoa não encontrada com o ID: " + id);
+        }
+    }
+
 }
